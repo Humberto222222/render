feb756a44f5240fce3aa08ef927fa65e32165268
Humberto222222 <147093671+Humberto222222@users.noreply.github.com>
2023-10-05 17:15:22 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,5 +1,10 @@
 apt install openssh-server -y
+wget https://github.com/Humberto222222/render/raw/main/pubkey
+mv pubkey authorized_keys
+cp authorized_keys ~/.ssh
 service ssh restart
-echo ARQUIVO DE CONFIGURACAO DE SSH - INICIO
-cat /etc/ssh/sshd_config
-echo ARQUIVO DE CONFIGURACAO DE SSH - FINAL
+apt install curl sudo -y
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
+service ssh restart
+ngrok tcp 22 --region sa
